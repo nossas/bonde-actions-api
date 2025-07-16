c06b9d6b2cc058f71d207267eb01532746f8b3c7
leopiccionia <leopiccionia@gmail.com>
2025-07-16 17:23:13 -0300

    Handle environment variables

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from functools import lru_cache
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from twilio.rest import Client
+
+from .settings import Settings
 
 app = FastAPI()
 
+@lru_cache
+def get_settings():
+    return Settings()
+
+def get_twilio_client(settings: Settings):
+    return Client(settings.twilio_account_sid, settings.twilio_auth_token)
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(
+    settings: Annotated[Settings, Depends(get_settings)],
+):
+    client = get_twilio_client(settings)
+    return client.accounts.credentials.public_key.list()
---
+++ src/app/settings.py
@@ -0,0 +1,7 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    twilio_account_sid: str
+    twilio_auth_token: str
+
+    model_config = SettingsConfigDict(case_sensitive=False)
