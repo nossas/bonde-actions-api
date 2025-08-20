710d53c85574e27c26d3be79fdde4ac4d50042df
Igor Santos <igr.exe@gmail.com>
2025-08-20 12:12:39 -0300

    fix(migrations): setup DATABASE_URL environment to run migrations

--- app/db.py
+++ app/db.py
@@ -4,11 +4,11 @@ from typing import Annotated
 from fastapi import Depends
 from sqlmodel import Session, create_engine
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+from app.config import settings
+
 
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(settings.database_url, connect_args=connect_args)
 
 def get_session():
     with Session(engine) as session:
